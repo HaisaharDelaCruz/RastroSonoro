fb9b5adfc2edaeafba2e236c564acec023442200
HaisaharDelaCruz <197O00983@itsx.edu.mx>
2025-08-10 21:53:44 -0600

    cambio en la url

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://rastrosonoro.com',
+    site: 'https://haisahardelacruz.github.io',
     output: 'static',
 });
